2edc7d93cdbed839b5783525c369d1cfcf27fc4a
U-Zyn Chua <chua@uzyn.com>
2020-06-21 00:28:25 +0800

    getDecimals() util function

---
+++ util.ts
@@ -0,0 +1,12 @@
+export function getDecimals(n: number | string): number {
+  if (isNaN(n as any)) {
+    throw new Error("InvalidNumber");
+  }
+  const components = n.toString().split(".");
+  if (components.length === 1) {
+    return 0;
+  } else if(components.length !== 2) {
+    throw new Error("InvalidNumber");
+  }
+  return components[1].length || 0; 
+}
